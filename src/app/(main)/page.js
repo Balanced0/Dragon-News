6f0551c61f2e367c2a5af43ea1da64d9779b2e75
balanced0 <alvichowdhury013@gmail.com>
2026-04-30 12:24:23 +0600

    Implement category fetching and update Home component layout

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,7 +1,27 @@
 import Image from "next/image";
 
-export default function Home() {
+async function getCategories(){
+  const res = await fetch("https://openapi.programming-hero.com/api/news/categories");
+  const data = await res.json();
+  return data.data.news_category;
+}
+
+export default async function Home() {
+  const categories = await getCategories();
+  console.log(categories);
   return (
-    <h1 className ="text-red-600">Hello</h1>
+    <div className="container mx-auto p-4 grid grid-cols-12 gap-4">
+      <div className="col-span-3 font-bold">All Category
+        {
+          categories.map((data) =>{
+            return <div key={data.category_id} className="mt-2 bg-gray-300 p-4">
+              {data.category_name}
+            </div>
+          })
+        }
+      </div>
+      <div className="bg-green-400 col-span-6 font-bold">Dragon News Home</div>
+      <div className="bg-purple-400 col-span-3 font-bold">Login With</div>
+    </div>
   );
 }
